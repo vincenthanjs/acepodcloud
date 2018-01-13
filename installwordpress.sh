0696890e78b00885075b776a7e1e822669886763
vincenthanjs <vincent@acepod.com>
2018-01-13 10:29:13 +0800

    Update installwordpress.sh

--- installwordpress.sh
+++ installwordpress.sh
@@ -3,7 +3,7 @@
 yum install php-gd -y
 service httpd restart
 cd ~
-wget http://wordpress.org/latest.tar.gz
+wget http://wordpress.org/latest.tar.gz  --no-check-certificate
 tar xzvf latest.tar.gz
 rsync -avP ~/wordpress/ /var/www/html/
 mkdir /var/www/html/wp-content/uploads
